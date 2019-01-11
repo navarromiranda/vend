60e928ff2cb4a1a503a8de9c5559d90d2a5f3311
Mauricio Navarro Miranda <mauricio@navarromiranda.mx>
2019-01-10 22:01:15 -0600

    Renombro Tab3 a Tickets

--- src/app/tickets/tickets.page.spec.ts
+++ src/app/tickets/tickets.page.spec.ts
@@ -1,21 +1,21 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { Tab3Page } from './tab3.page';
+import { TicketsPage } from './tickets.page';
 
-describe('Tab3Page', () => {
-  let component: Tab3Page;
-  let fixture: ComponentFixture<Tab3Page>;
+describe('TicketsPage', () => {
+  let component: TicketsPage;
+  let fixture: ComponentFixture<TicketsPage>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [Tab3Page],
+      declarations: [TicketsPage],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(Tab3Page);
+    fixture = TestBed.createComponent(TicketsPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
---
+++ src/app/tickets/tickets.page.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tickets',
+  templateUrl: 'tickets.page.html',
+  styleUrls: ['tickets.page.scss']
+})
+export class TicketsPage {}
