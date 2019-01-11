c367c23379975769c5de0bac50a9e949a72e7306
Mauricio Navarro Miranda <mauricio@navarromiranda.mx>
2019-01-11 14:00:29 -0600

    Configuro API url y jwt en el environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  api: {
+    tokens: {
+      prods: `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.
+      eyJpZCI6dHJ1ZSwiY2FwYWJpbGl0aWVzIjpbInJlYWRfcHJvZHMiXX0.
+      B5KtPu3b8PyK52ve-bXQJ1FusY70h9EmpKciGiKyWzI`.replace(/\s/g, '')
+    },
+    url: 'https://api.dulcerias.navarromiranda.mx'
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  api: {
+    tokens: {
+      prods: `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.
+      eyJpZCI6dHJ1ZSwiY2FwYWJpbGl0aWVzIjpbInJlYWRfcHJvZHMiXX0.
+      B5KtPu3b8PyK52ve-bXQJ1FusY70h9EmpKciGiKyWzI`.replace(/\s/g, '')
+    },
+    url: 'https://api.dulcerias.navarromiranda.mx'
+  },
   production: false
 };
 
