6e0444b0354c468ce4892258d2d5af8a11a9de6f
Mauricio Navarro Miranda <mauricio@navarromiranda.mx>
2019-01-15 21:09:38 -0600

    Vista elemental de favs

--- src/app/favs/favs.page.ts
+++ src/app/favs/favs.page.ts
@@ -1,8 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { ProdsService } from '../services/prods.service';
+import { IonSlides } from '@ionic/angular';
 
 @Component({
   selector: 'app-favs',
   templateUrl: 'favs.page.html',
   styleUrls: ['favs.page.scss']
 })
-export class FavsPage {}
+export class FavsPage {
+  @ViewChild('favsSlider') slider: IonSlides;
+  prods = {};
+  tags: Set<string> = new Set();
+
+  constructor(public Prods: ProdsService) {
+    // Obtengo todos los Prods
+    this.Prods.load()
+      .subscribe(_prods => {
+        // Me quedo sólo con los que tienen marca de favorito
+        const prods = _prods.filter(p => p.fav);
+
+        // Obtengo la lista ordenada de líneas
+        const sortedTags = prods.map(p => p.linea).sort();
+        this.tags = new Set(sortedTags);
+
+        // Creo un mapa de productos, agrupados por línea.
+        this.tags.forEach(tag => {
+          // Filtro los prods, para obtener sólo los de la linea `tag`.
+          this.prods[tag] = prods.filter(p => p.linea === tag);
+        });
+      });
+  }
+}
