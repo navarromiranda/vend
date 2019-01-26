b7e761dcdd4a520a1d8ffbf10163b86b98850ca1
Mauricio Navarro Miranda <mauricio@navarromiranda.mx>
2019-01-26 17:49:14 -0600

    Vista más chida de Favs

--- src/app/favs/favs.page.ts
+++ src/app/favs/favs.page.ts
@@ -1,6 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import { ProdsService } from '../services/prods.service';
-import { IonSlides } from '@ionic/angular';
+import { Prod } from '../services/prod';
 
 @Component({
   selector: 'app-favs',
@@ -8,26 +8,48 @@ import { IonSlides } from '@ionic/angular';
   styleUrls: ['favs.page.scss']
 })
 export class FavsPage {
-  @ViewChild('favsSlider') slider: IonSlides;
   prods = {};
-  tags: Set<string> = new Set();
+  tags: Set<string>;
+  tag = '';
+  brand = '';
+
+  txTags = {};
 
   constructor(public Prods: ProdsService) {
     // Obtengo todos los Prods
     this.Prods.load()
-      .subscribe(_prods => {
-        // Me quedo sólo con los que tienen marca de favorito
-        const prods = _prods.filter(p => p.fav);
-
+      .subscribe(prods => {
         // Obtengo la lista ordenada de líneas
         const sortedTags = prods.map(p => p.linea).sort();
         this.tags = new Set(sortedTags);
+        // "Selecciono" la primera linea, para la vista inicial
+        this.tag = sortedTags[0];
 
         // Creo un mapa de productos, agrupados por línea.
         this.tags.forEach(tag => {
           // Filtro los prods, para obtener sólo los de la linea `tag`.
           this.prods[tag] = prods.filter(p => p.linea === tag);
+          // Obtengo las marcas de los productos de la linea `tag`
+          const marcas: string[] = this.prods[tag].map((p: Prod) => p.marca).sort();
+          // Guardo esta relación, para usarla en la navegación
+          this.txTags[tag] = new Set(marcas);
         });
       });
+  }
+
+  tagChanged(ev: any) {
+    const value = ev.detail.value;
+    this.tag = value;
+    this.brand = '';
+  }
+
+  brandChanged(ev: any) {
+    const value = ev.detail.value;
+    this.brand = value;
+  }
+
+  prodsByBrand(prods: Prod[], brand: string): Prod[] {
+    if (!brand || '' === brand) { return prods; }
+    return prods.filter(prod => prod.marca === brand);
   }
 }
