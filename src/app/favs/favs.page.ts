65f4177438dac4d10f17657827dad41d88390802
Mauricio Navarro Miranda <mauricio@navarromiranda.mx>
2019-01-26 21:54:38 -0600

    Favs: agrego refresher

--- src/app/favs/favs.page.ts
+++ src/app/favs/favs.page.ts
@@ -10,12 +10,33 @@ import { Prod } from '../services/prod';
 export class FavsPage {
   prods = {};
   tags: Set<string>;
-  tag = '';
-  brand = '';
+  tag = undefined;
+  brand = undefined;
 
   txTags = {};
 
   constructor(public Prods: ProdsService) {
+    const fakeEvent = { target: { complete: () => { } } }
+    this.doRefresh(fakeEvent);
+  }
+
+  tagChanged(ev: any) {
+    const value = ev.detail.value;
+    this.tag = value;
+    this.brand = undefined;
+  }
+
+  brandChanged(ev: any) {
+    const value = ev.detail.value;
+    this.brand = value;
+  }
+
+  prodsByBrand(prods: Prod[], brand: string): Prod[] {
+    if (!brand) { return prods; }
+    return prods.filter(prod => prod.marca === brand);
+  }
+
+  doRefresh(event) {
     // Obtengo todos los Prods
     this.Prods.load()
       .subscribe(prods => {
@@ -23,7 +44,7 @@ export class FavsPage {
         const sortedTags = prods.map(p => p.linea).sort();
         this.tags = new Set(sortedTags);
         // "Selecciono" la primera linea, para la vista inicial
-        this.tag = sortedTags[0];
+        this.tag = this.tag || sortedTags[0];
 
         // Creo un mapa de productos, agrupados por línea.
         this.tags.forEach(tag => {
@@ -33,23 +54,8 @@ export class FavsPage {
           const marcas: string[] = this.prods[tag].map((p: Prod) => p.marca).sort();
           // Guardo esta relación, para usarla en la navegación
           this.txTags[tag] = new Set(marcas);
+          event.target.complete();
         });
       });
-  }
-
-  tagChanged(ev: any) {
-    const value = ev.detail.value;
-    this.tag = value;
-    this.brand = '';
-  }
-
-  brandChanged(ev: any) {
-    const value = ev.detail.value;
-    this.brand = value;
-  }
-
-  prodsByBrand(prods: Prod[], brand: string): Prod[] {
-    if (!brand || '' === brand) { return prods; }
-    return prods.filter(prod => prod.marca === brand);
   }
 }
