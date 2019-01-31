cc51ffcbbd2233f833078f31da5da1148203a14c
Mauricio Navarro Miranda <mauricio@navarromiranda.mx>
2019-01-30 19:11:21 -0600

    Refactorizo TicketsService: El newTicket usa prod.articulo como llave, en lugar del objeto completo.

--- src/app/services/tickets.service.ts
+++ src/app/services/tickets.service.ts
@@ -1,36 +1,44 @@
 import { Injectable } from '@angular/core';
 import { Prod, Clave } from './prod';
 
+interface ITicketrow {
+  prod: Prod;
+  qty: number;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class TicketsService {
-  public newTicket = new Map<Prod, number>();
+  public newTicket = new Map<string, ITicketrow>();
 
   constructor() { }
 
   addProdToTicket(prod: Prod): void {
-    const curr = this.newTicket.get(prod) || 0;
-    this.newTicket.set(prod, 1 + curr);
+    const key = prod.articulo;
+    const qty = (this.newTicket.get(key) || { prod: undefined, qty: 0 }).qty;
+    this.newTicket.set(key, { prod, qty: 1 + qty });
   }
 
   minusOneProdToTicket(prod: Prod): void {
-    const curr = this.newTicket.get(prod) || 0;
-    if (curr > 1) {
-      this.newTicket.set(prod, -1 + curr);
+    const key = prod.articulo;
+    const qty = (this.newTicket.get(key) || { prod: undefined, qty: 0 }).qty;
+    if (qty > 1) {
+      this.newTicket.set(key, { prod, qty: -1 + qty });
     } else {
       this.deleteProdFromTicket(prod);
     }
   }
 
-  deleteProdFromTicket(prod): void {
-    this.newTicket.delete(prod);
+  deleteProdFromTicket(prod: Prod): void {
+    const key = prod.articulo;
+    this.newTicket.delete(key);
   }
 
   getNewTicketQty(): number {
     let qty = 0;
-    this.newTicket.forEach((q, prod) => {
-      qty += q;
+    this.newTicket.forEach(row => {
+      qty += row.qty;
     });
 
     return qty;
@@ -38,11 +46,17 @@ export class TicketsService {
 
   getNewTicketTotal(): number {
     let total = 0;
-    this.newTicket.forEach((qty, prod) => {
-      total += (qty * prod.precio);
+    this.newTicket.forEach(row => {
+      total += (row.qty * row.prod.precio);
     });
 
     return total;
+  }
+
+  getProdQtyOnTicket(prod: Prod): number {
+    const key = prod.articulo;
+    const qty = (this.newTicket.get(key) || { prod: undefined, qty: undefined }).qty;
+    return qty;
   }
 
 }
