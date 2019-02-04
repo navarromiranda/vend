b1262e1aac9e2feffe23cf951b94df705d49d26d
Mauricio Navarro Miranda <mauricio@navarromiranda.mx>
2019-02-04 15:49:42 -0600

    Muestro un toast con la versión c:

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,21 @@
 import { Component } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
+import { Platform, ToastController } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { VERSION } from 'src/environments/version';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
+  version = `${VERSION.semver.raw}+${VERSION.distance}`;
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private toast: ToastController
   ) {
     this.initializeApp();
   }
@@ -21,6 +24,29 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      this.presentToast();
     });
+  }
+
+  async presentToast() {
+    const version = localStorage.getItem('version');
+    localStorage.setItem('version', this.version);
+
+    let duration = 2000;
+    let message = `Vend ${this.version}`;
+
+    if (this.version !== version) {
+      duration = 5000;
+      message = `Nueva versión ${this.version}`;
+    }
+
+    const toast = await this.toast.create({
+      animated: true,
+      duration,
+      message,
+      position: 'middle',
+      translucent: true
+    });
+    toast.present();
   }
 }
