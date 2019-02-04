4f7334bbd14ee38098391dd5f6a0119392269d4a
Mauricio Navarro Miranda <mauricio@navarromiranda.mx>
2019-02-04 15:31:05 -0600

    prestart, prebuild scripts

---
+++ src/version.js
@@ -0,0 +1,17 @@
+const { gitDescribeSync } = require('git-describe');
+const { version } = require('../package.json');
+const { resolve, relative } = require('path');
+const { writeFileSync } = require('fs');
+
+const gitInfo = gitDescribeSync({
+    dirtyMark: false,
+    dirtySemver: false
+});
+
+gitInfo.version = version;
+
+const file = resolve(__dirname, '..', 'src', 'environments', 'version.ts');
+writeFileSync(file,
+    `export const VERSION = ${JSON.stringify(gitInfo, null, 4)};`, { encoding: 'utf-8' });
+
+console.log(`Wrote version info ${gitInfo.raw} to ${relative(resolve(__dirname, '..'), file)}`);
