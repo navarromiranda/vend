59cb1bd46d00e8e8d58ea47e2764feff4db6c06a
Mauricio Navarro Miranda <mauricio@navarromiranda.mx>
2019-02-04 16:16:25 -0600

    Agrego logs en el script de versión

--- src/version.js
+++ src/version.js
@@ -1,17 +1,18 @@
-const { gitDescribeSync } = require('git-describe');
-const { version } = require('../package.json');
-const { resolve, relative } = require('path');
-const { writeFileSync } = require('fs');
+const { gitDescribeSync } = require('git-describe')
+const { version } = require('../package.json')
+const { resolve, relative } = require('path')
+const { writeFileSync } = require('fs')
 
 const gitInfo = gitDescribeSync({
-    dirtyMark: false,
-    dirtySemver: false
-});
+  dirtyMark: false,
+  dirtySemver: false
+})
 
-gitInfo.version = version;
+gitInfo.version = version
 
-const file = resolve(__dirname, '..', 'src', 'environments', 'version.ts');
+const file = resolve(__dirname, '..', 'src', 'environments', 'version.ts')
 writeFileSync(file,
-    `export const VERSION = ${JSON.stringify(gitInfo, null, 4)};`, { encoding: 'utf-8' });
+  `export const VERSION = ${JSON.stringify(gitInfo, null, 4)};`, { encoding: 'utf-8' })
 
-console.log(`Wrote version info ${gitInfo.raw} to ${relative(resolve(__dirname, '..'), file)}`);
+console.log(`Wrote version info ${gitInfo.raw} to ${relative(resolve(__dirname, '..'), file)}`)
+console.log(`Wrote ${JSON.stringify(gitInfo)} to ${relative(resolve(__dirname, '..'), file)}`)
