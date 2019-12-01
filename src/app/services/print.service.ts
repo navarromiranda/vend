3b89252fce6cc8177fb3a4abe26ec864a4ca5513
Mauricio Navarro Miranda <mauricio@navarromiranda.mx>
2019-11-30 23:07:52 -0600

    refactor: divido print() en dos para facilitar las pruebas unitarias

--- src/app/services/print.service.ts
+++ src/app/services/print.service.ts
@@ -25,11 +25,20 @@ export class PrintService {
   }
 
   async print(ticket: Map<string, ITicketrow>, date: Date, pago: number, uuid: string) {
+    const lines = this.build(ticket, date, pago, uuid).split('\n');
+
+    for (const line of lines) {
+      await this.row(line);
+    }
+  }
+
+  build(ticket: Map<string, ITicketrow>, date: Date, pago: number, uuid: string): string {
+    const doc = [];
     const fecha = moment(date).format('YYYY/MM/DD HH:mm')
 
-    await this.row(this.pad('Dulcerias N/M', 31))
-    await this.row(this.pad('Francisco I. Madero 35', 31))
-    await this.row('\n\n')
+    doc.push(this.pad('Dulcerias N/M', 31))
+    doc.push(this.pad('Francisco I. Madero 35', 31))
+    doc.push('\n\n')
 
     const rows = Array.from(ticket.values())
       .map(({ prod, qty }) => ({
@@ -66,28 +75,30 @@ export class PrintService {
       }))
 
     for (const { qty, descripcion, precio, subtotal } of lines) {
-      await this.row(`${qty} ${descripcion}${Number(qty) === 1 ? '' : ' ' + precio} ${subtotal}`)
+      doc.push(`${qty} ${descripcion}${Number(qty) === 1 ? '' : ' ' + precio} ${subtotal}`)
     }
 
-    await this.row('')
-    await this.row(piezas.padStart(31, '    PIEZAS'.padEnd(31)))
-    await this.row(total.padStart(31, '     TOTAL'.padEnd(31)))
+    doc.push('')
+    doc.push(piezas.padStart(31, '    PIEZAS'.padEnd(31)))
+    doc.push(total.padStart(31, '     TOTAL'.padEnd(31)))
 
     if (Number(pago) > Number(total)) {
       const PAGO = pago.toFixed(totalDecimals)
       const CAMBIO = (pago - Number(total)).toFixed(totalDecimals)
 
-      await this.row(PAGO.padStart(31, '  EFECTIVO'.padEnd(31)))
-      await this.row(CAMBIO.padStart(31, '    CAMBIO'.padEnd(31)))
+      doc.push(PAGO.padStart(31, '  EFECTIVO'.padEnd(31)))
+      doc.push(CAMBIO.padStart(31, '    CAMBIO'.padEnd(31)))
     }
 
-    await this.row('')
-    await this.row(fecha.padStart(31, '     FECHA'.padEnd(31)))
-    await this.row(uuid.substring(0, 18).padStart(31, '    TICKET'.padEnd(31)))
+    doc.push('')
+    doc.push(fecha.padStart(31, '     FECHA'.padEnd(31)))
+    doc.push(uuid.substring(0, 18).padStart(31, '    TICKET'.padEnd(31)))
 
-    await this.row('\n\n');
-    await this.row('Gracias por tu compra');
-    await this.row('\n\n');
+    doc.push('\n\n');
+    doc.push('Gracias por tu compra');
+    doc.push('\n\n');
+
+    return doc.join('\n');
   }
 
   private pad = (s: string, l: number, c: string = ' ') => s.padStart((s.length + l) / 2, c).padEnd(l, c);
